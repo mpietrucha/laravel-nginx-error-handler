041b4f67356858b263d61355a8ad91db3c32b866
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-04-06 19:35:09 +0200

    wip

--- src/NginxErrorServiceProvider.php
+++ src/NginxErrorServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Mpietrucha\Nginx\Error;
 
 use Illuminate\Support\ServiceProvider;
+use Mpietrucha\Composer\Composer;
 use Mpietrucha\Nginx\Error\Console\Commands;
 
 class NginxErrorServiceProvider extends ServiceProvider
@@ -12,6 +13,12 @@ class NginxErrorServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/nginx.php' => config_path('nginx.php'),
         ], 'config');
+
+        Composer::after()->artisan('nginx:install');
+
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
 
         $this->commands([
             Commands\InstallCommand::class,
